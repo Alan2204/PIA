f3490be50121ff553a3b6e7c5aee20bc5dc3893b
Alan <alan_darkstar@hotmail.com>
2023-05-07 21:26:33 -0600

    first commit

---
+++ WebApiCitasMedicas/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCitasMedicas
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ WebApiCitasMedicas/Entidades/Citas.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApiCitasMedicas.Entidades
+{
+    public class Citas
+    {
+    }
+}
